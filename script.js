0b78e620f3287fb28268e2d6d2beb638066bc521
drogosenturi <violoncello82@gmail.com>
2025-04-05 16:25:42 -0500

    added html to support slideshow

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 console.log("JS loaded")
+/* hamburger menu toggle */
 document.querySelector('.hamburger').addEventListener('click',function(){
     const mobileMenu = document.querySelector('.mobile_menu');
     mobileMenu.classList.toggle('active');
     console.log("clicked!");
 })
+
+/* slideshow */
