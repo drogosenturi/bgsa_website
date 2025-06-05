65edc74e1cfe365f2396503c4678216509988a88
drogosenturi <violoncello82@gmail.com>
2025-06-05 15:26:34 -0500

    touchswipe functionality and newsletter/events update

---
+++ scripts/pageloader.js
@@ -0,0 +1,100 @@
+// attempt at making dynamic pageloader
+let pagename = '';
+const newsletter = document.querySelector("#newsletter")
+function loadPage (partial, target) {
+    const page = window.location.hash.substring(1) || 'home';
+    pagename = page;
+    fetch(`./partials/${partial}.html`)
+    .then(response => response.text())
+    .then(html => document.getElementById(target).innerHTML = html);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    newsletter.addEventListener('click', loadPage('newsletter','content'));
+    loadPage('home', 'content');
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (window.location.hash === '/#home') {
+        const currentSlide = document.querySelector("#full-width-image");
+        const selector = document.querySelector("#selector");
+        const pics = ["../media/misc_photos/board_and_officers_plain.png", "../media/misc_photos/election.jpg",
+            "../media/misc_photos/ssa_lively.jpg","../media/misc_photos/ssa_wide.jpg",
+            "../media/misc_photos/swap.jpg"];
+        const glowy = ["../media/logos/silde-on.png","../media/logos/silde-off.png"];
+        let active = 0;
+        let arr = [].slice.call(selector.children)
+
+
+        function makeDots() {
+            for (i = 0; i < pics.length; i++) {
+            const dot = document.createElement('img');
+            if (i === 0) {
+                dot.setAttribute('src',glowy[0]);
+                dot.dataset.on = true;
+            } else {
+                dot.setAttribute('src',glowy[1]);
+                dot.dataset.on = false;
+            }
+            selector.appendChild(dot);  
+            }   
+        }
+
+        function select(e, a) {
+            for (i of selector.children) {
+                i.src = glowy[1];
+                i.dataset.on = false;
+            }
+            if (a) {
+                selector.children[a].src = glowy[0];
+                selector.children[a].dataset.on = true;
+            } else if (a === 0) {
+                selector.children[0].src = glowy[0];
+                selector.children[0].dataset.on = true;
+            } else {
+                e.target.src = glowy[0];
+                e.target.dataset.on = true;
+            }
+            arr = [].slice.call(selector.children)
+            setActive(a);
+        }
+
+        function setActive(a) {
+            if (a) {
+                active = a
+            } else {
+                for (const i in arr) {
+                    if (arr[i].dataset.on === "true") {
+                        active = Number(i);
+                        console.log(i);
+                    }
+                    console.log(arr[i].dataset.on);
+                }
+            }
+            slideshow();
+        }
+
+        function slideshow() {
+            currentSlide.src = pics[active];
+        }
+
+        function rotate() {
+            if (active < pics.length - 1) {
+                active += 1;
+            } else {
+                active = 0;
+            }
+            select(1,active)
+
+        }
+
+        function swipes() {
+            currentSlide.addEventListener('touchend', rotate);
+        }
+
+        makeDots;
+        selector.addEventListener('click',select);
+        currentSlide.addEventListener('touchmove', swipes);
+        setInterval(rotate, 5000);
+    }
+})
---
+++ scripts/script.js
@@ -0,0 +1,96 @@
+console.log("JS loaded")
+/* hamburger menu toggle */
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener('click',() => {
+    const mobileMenu = document.querySelector('.mobile_menu');
+    mobileMenu.classList.toggle('active');
+    console.log("clicked!");
+})
+
+/* slideshow */
+const currentSlide = document.querySelector("#full-width-image");
+const selector = document.querySelector("#selector");
+const pics = ["../media/misc_photos/board_and_officers_plain.png", 
+    "../media/misc_photos/election.jpg", "../media/misc_photos/ssa_lively.jpg",
+    "../media/misc_photos/ssa_wide.jpg", "../media/misc_photos/swap.jpg"];
+const glowy = ["../media/logos/silde-on.png","../media/logos/silde-off.png"];
+let active = 0;
+let arr = [].slice.call(selector.children) // access a list of the selector's children
+
+// create the dots based on how many pictures there are,
+// set the active dot as glowy
+for (i = 0; i < pics.length; i++) {
+const dot = document.createElement('img');
+if (i === 0) {
+    dot.setAttribute('src',glowy[0]);
+    dot.dataset.on = true;
+} else {
+    dot.setAttribute('src',glowy[1]);
+    dot.dataset.on = false;
+}
+selector.appendChild(dot);  
+}   
+
+// respond to click event, set clicked dot to glowy
+// if not a click event, active will increment and set next dot glowy
+function select(e, a) {
+    for (i of selector.children) {
+        i.src = glowy[1];
+        i.dataset.on = false;
+    }
+    if (a) {
+        selector.children[a].src = glowy[0];
+        selector.children[a].dataset.on = true;
+    } else if (a === 0) {
+        selector.children[0].src = glowy[0];
+        selector.children[0].dataset.on = true;
+    } else {
+        e.target.src = glowy[0];
+        e.target.dataset.on = true;
+    }
+    arr = [].slice.call(selector.children)
+    setActive(a);
+}
+
+// set active to the correct picture
+function setActive(a) {
+    if (a) {
+        active = a
+    } else {
+        for (const i in arr) {
+            if (arr[i].dataset.on === "true") {
+                active = Number(i);
+                console.log(i);
+            }
+            console.log(arr[i].dataset.on);
+        }
+    }
+    slideshow();
+}
+
+// change current slide to the active picture
+function slideshow() {
+    currentSlide.src = pics[active];
+}
+
+// move to the next slide
+function rotate() {
+    if (active < pics.length - 1) {
+        active += 1;
+    } else {
+        active = 0;
+    }
+    select(1,active)
+
+}
+
+// initiates through touch move, when finger is lifted
+// the rotate function is called
+function swipes() {
+    currentSlide.addEventListener('touchend', rotate);
+}
+
+//makeDots;
+selector.addEventListener('click',select);
+currentSlide.addEventListener('touchmove', swipes);
+setInterval(rotate, 4000);
