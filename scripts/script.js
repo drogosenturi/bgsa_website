55dfff6b169d6c7b0bf303c82ffe47770227579d
drogosenturi <violoncello82@gmail.com>
2025-07-17 18:28:49 -0500

    updates for newsletter, events, small js changes

--- scripts/script.js
+++ scripts/script.js
@@ -10,26 +10,28 @@ hamburger.addEventListener('click',() => {
 /* slideshow */
 const currentSlide = document.querySelector("#full-width-image");
 const selector = document.querySelector("#selector");
-const pics = ["../media/misc_photos/board_and_officers_plain.png", 
-    "../media/misc_photos/election.jpg", "../media/misc_photos/ssa_lively.jpg",
-    "../media/misc_photos/ssa_wide.jpg", "../media/misc_photos/swap.jpg"];
+const pics = ["../media/misc_photos/ssa_good.jpg", "../media/misc_photos/ssa_lively.jpg", 
+    "../media/misc_photos/swap.jpg", "../media/misc_photos/ssa_wide.jpg", 
+    "../media/misc_photos/election.jpg", "../media/misc_photos/board_and_officers_plain.png"];
 const glowy = ["../media/logos/silde-on.png","../media/logos/silde-off.png"];
 let active = 0;
-let arr = [].slice.call(selector.children) // access a list of the selector's children
+let arr = [].slice.call(selector.children); // access a list of the selector's children
 
 // create the dots based on how many pictures there are,
 // set the active dot as glowy
-for (i = 0; i < pics.length; i++) {
-const dot = document.createElement('img');
-if (i === 0) {
-    dot.setAttribute('src',glowy[0]);
-    dot.dataset.on = true;
-} else {
-    dot.setAttribute('src',glowy[1]);
-    dot.dataset.on = false;
+function makeDots() {
+    for (i = 0; i < pics.length; i++) {
+    const dot = document.createElement('img');
+    if (i === 0) {
+        dot.setAttribute('src',glowy[0]);
+        dot.dataset.on = true;
+    } else {
+        dot.setAttribute('src',glowy[1]);
+        dot.dataset.on = false;
+    }
+    selector.appendChild(dot);  
+    }
 }
-selector.appendChild(dot);  
-}   
 
 // respond to click event, set clicked dot to glowy
 // if not a click event, active will increment and set next dot glowy
@@ -90,7 +92,7 @@ function swipes() {
     currentSlide.addEventListener('touchend', rotate);
 }
 
-//makeDots;
+makeDots()
 selector.addEventListener('click',select);
 currentSlide.addEventListener('touchmove', swipes);
-setInterval(rotate, 4000);
+const rotateTimer = setInterval(rotate, 4000);
